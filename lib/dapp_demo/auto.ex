0705a3f32e374d9482634ae85116297d7a5c6e2b
Wells Qiu <wellsqiu@arpnetwork.org>
2018-08-28 15:56:08 +0800

    Fix Auto module bug.

--- lib/dapp_demo/auto.ex
+++ lib/dapp_demo/auto.ex
@@ -1,17 +1,12 @@
 defmodule DappDemo.Auto do
   require Logger
 
-  alias DappDemo.ServerRegistry
-  alias DappDemo.DevicePool
+  alias DappDemo.{Config, DevicePool, Server, ServerRegistry}
 
   use GenServer
 
   @check_interval 10000
   @total_device 100
-  @price Application.get_env(:dapp_demo, :price)
-  @ip Application.get_env(:dapp_demo, :ip)
-  @port Application.get_env(:dapp_demo, :port)
-  @app_data_file2 Application.get_env(:dapp_demo, :app_data_file2)
 
   def start_link(_opts) do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
@@ -22,8 +17,19 @@ defmodule DappDemo.Auto do
   end
 
   def init(_opts) do
+    installed = %{}
+
+    Process.send_after(self(), :check_interval, @check_interval)
+    {:ok, installed}
+  end
+
+  def handle_cast({:install_finish, address}, installed) do
+    {:noreply, Map.delete(installed, address)}
+  end
+
+  def handle_info(:check_interval, installed) do
     apps =
-      with {:ok, data} <- File.read(@app_data_file2),
+      with {:ok, data} <- File.read(Config.get(:app_data_file2)),
            {:ok, list} <- Poison.decode(data) do
         list
       else
@@ -31,23 +37,17 @@ defmodule DappDemo.Auto do
           []
       end
 
-    installed = %{}
-
-    Process.send_after(self(), :check_interval, @check_interval)
-    {:ok, {apps, installed}}
-  end
-
-  def handle_cast({:install_finish, address}, {apps, installed}) do
-    {:noreply, {apps, Map.delete(installed, address)}}
-  end
-
-  def handle_info(:check_interval, {apps, installed}) do
     servers = ServerRegistry.lookup_all()
     devices = DevicePool.lookup_all()
 
     Enum.each(servers, fn {_, pid} ->
       if length(devices) < @total_device do
-        DappDemo.Server.device_request(pid, @price, @ip, @port)
+        DappDemo.Server.device_request(
+          pid,
+          Config.get(:price),
+          Config.get(:ip),
+          Config.get(:port)
+        )
       end
     end)
 
@@ -65,10 +65,26 @@ defmodule DappDemo.Auto do
         if is_nil(dev.package) && !Map.has_key?(installed, dev.address) do
           {:ok, app} = Enum.fetch(apps, :rand.uniform(length(apps)) - 1)
 
-          if :ok == DappDemo.App.install(dev, app["package"], app["url"], app["size"], app["md5"]) do
-            Map.put(acc, dev.address, app["package"])
-          else
-            acc
+          try do
+            if :ok ==
+                 DappDemo.App.install(dev, app["package"], app["url"], app["size"], app["md5"]) do
+              Map.put(acc, dev.address, app["package"])
+            else
+              acc
+            end
+          rescue
+            e ->
+              Logger.warn(inspect(e))
+
+              case ServerRegistry.lookup(dev.server_address) do
+                {:ok, pid} ->
+                  Server.device_release(pid, dev.address)
+
+                _ ->
+                  nil
+              end
+
+              acc
           end
         else
           acc
@@ -77,11 +93,6 @@ defmodule DappDemo.Auto do
 
     Process.send_after(self(), :check_interval, @check_interval)
 
-    {:noreply, {apps, installed}}
-  rescue
-    e ->
-      Logger.warn(inspect(e))
-      Process.send_after(self(), :check_interval, @check_interval)
-      {:noreply, {apps, installed}}
+    {:noreply, installed}
   end
 end
